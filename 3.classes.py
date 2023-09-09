618ba1ece8d24c935ecf7f7cf175918dbc5d2ffe
Auryx <lilbizb1@gmail.com>
2023-09-09 09:44:59 -1000

    finished classes.py

--- 3.classes.py
+++ 3.classes.py
@@ -1,16 +1,25 @@
-
+import random
 # Go back to Alex's lesson on Wednesday August 30th as a guide
 
 # 1. Create a class called Wolf. When this class is instantiated it takes in a name and age. 
 # the class is also to have a method called back which will print its name and 'Ahhhoooo'
-
+class Wolf:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def bark(name):
+        print(f'{name.name} goes Ahhhoooo')
 # 2. Instantiate: Create an Object from the Wolf class and use the bark method
-
+george = Wolf("George", 10)
+george.bark()
 # 3. Create a class called Dog. This class will Inherit from the class Wolf. 
-# Do not define any methods
-
+# Please define a method called 'fetch' and have it print 'who is a good boy'
+class Dog(Wolf):
+    def fetch():
+        print('who is a good boy')
 # 4. Instantiate: Create an Object from the Dog class and try the bark method
-
+fred = Dog("Fred", 7)
+fred.bark()
 # 5. Remember the class Fighter from Aug 30th.
 # Change the attack method. 
 # In the attack method, damage is strength subtracted by defense. 
@@ -30,13 +39,13 @@ class Fighter:
         self.defence = defence
         
     def attack(self, opponent):
-        damage = self.strength - opponent.defence
+        damage = round(random.randint(0, self.strength)) - opponent.defence
         if (damage < 0):
             damage = 0
         opponent.hp -= damage
         print(f"{self.name} attacks {opponent.name} for {damage} damage")
         
-ryu = Fighter("Ryu", 100, 10, 5)
+ryu = Fighter("Ryu", 100, 12, 3)
 ken = Fighter("Ken", 100, 12, 3)
 
 while(True):
